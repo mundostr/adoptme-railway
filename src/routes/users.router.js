d0ac95ba21e7e66913e92be28543d1b7e138ab8b
Carlos Perren <idux.net@gmail.com>
2023-08-29 20:55:05 -0300

    Cambio nombre plantilla listado usuarios

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/', usersController.getAllUsers);
 router.get('/:uid', usersController.getUser);
-router.get('/show/all', usersController.showUsers);
+router.get('/show/complete', usersController.showUsers);
 router.put('/:uid', usersController.updateUser);
 router.delete('/:uid', usersController.deleteUser);
 
